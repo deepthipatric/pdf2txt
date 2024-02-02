0a823ea25fd5904de3d4c23424bc1c4767477aee
deepthipatric <33624842+deepthipatric@users.noreply.github.com>
2024-02-02 13:04:57 +0100

    Update unique_words.R

--- unique_words.R
+++ unique_words.R
@@ -1,4 +1,4 @@
-# Themes with words
+# themes with words
 themes <- list(
   'Human Interest' = c('sympathy', 'motivation', 'problems', 'concerns', 'achievements', 'curiosity', 'feelings', 'compassion', 'result', 'consequence', 'upshot', 'outcome', 'out-turn', 'sequel', 'reaction', 'repercussions', 'reverberations', 'ramifications', 'endresult', 'termination', 'culmination', 'denouement', 'corollary', 'concomitant', 'aftermath', 'footprint', 'product', 'by-product', 'sequelae', 'pay-off', 'issue', 'success'),
   'Attribute of Responsibility' = c('limitation of liability', 'allocating responsibilities', 'attributing liability', 'attribution of liability', 'chalk up', 'establish liability', 'held accountable', 'identify that responsible', 'imposition of liability', 'place responsibility', 'assign responsibility', 'attributing responsibility', 'given responsibility', 'place the responsibility', 'allocate responsibilities', 'allocate responsibility', 'allocation of liability', 'assign responsibilities', 'assignment of liability', 'assignment of responsibility', 'be responsible', 'channelling of liability'),
@@ -7,11 +7,11 @@ themes <- list(
   'Conflict' = c('dispute', 'quarrel', 'squabble', 'disagreement', 'difference of opinion', 'dissension', 'antagonism', 'violence they experience', 'blood', 'hostility', 'falling-out', 'disputation', 'contention', 'clash', 'shouting match', 'exchange war', 'fracas', 'opposite', 'climate and environmental crises')
 )
 
-# Concatenate all words into a single vector
+# concatenate all words into a single vector
 all_words <- unlist(themes)
 
-# Extract unique words
+# extract unique words
 unique_words <- unique(all_words)
 
-# Print unique words
+# output unique words
 print(unique_words)
