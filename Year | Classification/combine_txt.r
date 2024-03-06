4cc526153925b23862993f6606d1bcf462379d99
deepthipatric <33624842+deepthipatric@users.noreply.github.com>
2024-03-06 14:07:14 +0100

    Update combine_txt.r

--- Year | Classification/combine_txt.r
+++ Year | Classification/combine_txt.r
@@ -1,1 +1,9 @@
 #combines text files per year in the folder
+setwd("C:/Users/wolves/Desktop/Years data/Classification _ years/M&G/2013/")
+file_list <- list.files(pattern = "\\.txt$")  
+combined_file <- file("combined2013mg.txt", "w")
+for (file in file_list) {
+  content <- readLines(file)
+  writeLines(content, combined_file)
+}
+close(combined_file)
